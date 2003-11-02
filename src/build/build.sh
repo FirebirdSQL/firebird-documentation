22181992a9ddadf05b54102034376ccf796129d0
Paul Vinkenoog <paulvink@users.sourceforge.net>
2003-11-02 23:14:36 +0000

    Fixed Linux build.sh

--- src/build/build.sh
+++ src/build/build.sh
@@ -1,20 +1,30 @@
 #! /bin/sh
 
-# ----- ant libraries ------
-TARGET_CLASSPATH=../../lib/ant.jar:\
-../../lib/optional.jar:\
-# ----- saxon libraries ------
-../../lib/saxon.jar:\
-# ----- fop libraries ------
-../../lib/fop.jar:\
-../../lib/batik.jar:\
-../../lib/avalon-framework-cvs-20020315.jar:\
-../../lib
-
-if [ "$JAVA_HOME" != "" ] ; then
-   TARGET_CLASSPATH=$TARGET_CLASSPATH:$JAVA_HOME/lib/tools.jar
-else
-   echo "Error: The JAVA_HOME environment variable is not set."
+if [ "$JAVA_HOME" == "" ] ; then
+  echo "    Error: The JAVA_HOME environment variable is not set."
+  echo "    You must set it to point at your JDK or JRE distribution,"
+  echo "    e.g. JAVA_HOME=/usr/java/j2sdk"
+  exit 1
 fi
 
-java -Xmx100000000 -showversion -classpath $TARGET_CLASSPATH org.apache.tools.ant.Main $*
+
+# set up the classpath _CP_ :
+
+# ----- ant libraries: ------
+_CP_=../../lib/ant.jar
+_CP_=$_CP_:../../lib/optional.jar
+
+# ----- saxon libraries: ------
+_CP_=$_CP_:../../lib/saxon.jar
+
+# ----- FOP libraries: ------
+_CP_=$_CP_:../../lib/fop.jar
+_CP_=$_CP_:../../lib/batik.jar
+_CP_=$_CP_:../../lib/avalon-framework-cvs-20020315.jar
+
+_CP_=$_CP_:../../lib
+
+_CP_=$_CP_:$JAVA_HOME/lib/tools.jar
+
+java -Xmx100000000 -showversion -classpath $_CP_ org.apache.tools.ant.Main $*
+
