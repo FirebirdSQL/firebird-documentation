f17f253754a4d7a5ea7f1da5d5274636590e9abc
Popa Marius Adrian <mapopa@gmail.com>
2020-01-10 11:56:39 +0200

    change http to https

--- tools/get_tools_linux.sh
+++ tools/get_tools_linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget http://www.firebirdsql.org/doctools/docbook-dtd.zip
-wget http://www.firebirdsql.org/doctools/docbook-stylesheets.zip
+wget https://www.firebirdsql.org/doctools/docbook-dtd.zip
+wget https://www.firebirdsql.org/doctools/docbook-stylesheets.zip
 unzip docbook-dtd.zip
 unzip docbook-stylesheets.zip
