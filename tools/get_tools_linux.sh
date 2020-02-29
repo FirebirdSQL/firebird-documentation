9d48524c92915bdd48ebd90f46da3c65f8b51d9b
Mark Rotteveel <mark@lawinegevaar.nl>
2020-02-29 17:25:58 +0100

    Delete old instructions for tools/lib

--- tools/get_tools_linux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wget https://www.firebirdsql.org/doctools/docbook-dtd.zip
-wget https://www.firebirdsql.org/doctools/docbook-stylesheets.zip
-unzip docbook-dtd.zip
-unzip docbook-stylesheets.zip
