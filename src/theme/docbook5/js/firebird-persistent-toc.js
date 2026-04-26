e7657c3b27d758ffe9cdf4be5daed64175e37453
Mark Rotteveel <mark@lawinegevaar.nl>
2026-04-26 08:54:43 +0200

    Update docbook5 theme to DocBook xslTNG 2.7.1

--- src/theme/docbook5/js/firebird-persistent-toc.js
+++ src/theme/docbook5/js/firebird-persistent-toc.js
@@ -1,4 +1,4 @@
-/* This is based on persistent-toc.js DocBook xslTNG version 2.5.0, https://xsltng.docbook.org */
+/* This is based on persistent-toc.js DocBook xslTNG version 2.7.1, https://xsltng.docbook.org */
 (function() {
   const ESC = 27;
   const SPACE = 32;
@@ -7,7 +7,7 @@
   let borderLeftColor = "white";
   let curpress = null;
   let searchListener = false;
-  let VERSION = "2.5.0";
+  let VERSION = "2.7.1";
   let PTOCID = "ptoc-data-file";
 
   const showToC = function(event) {
